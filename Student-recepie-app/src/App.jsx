9a7206fe4183560818c12ef85d18ed9841ef6625
MarkovicMaks <markovic.maks@gmail.com>
2024-12-05 15:47:39 +0100

    F bootstrap

--- Student-recepie-app/src/App.jsx
+++ Student-recepie-app/src/App.jsx
@@ -1,29 +1,15 @@
-import { useState } from 'react'
-import viteLogo from '/vite.svg'
-import './App.css'
+import React from 'react';
+import Button from 'react-bootstrap/Button';
 
 function App() {
-  const [count, setCount] = useState(0)
-
   return (
     <>
-      <div>
-        
-      </div>
-      <h1>Vite + React</h1>
-      <div className="card">
-        <button onClick={() => setCount((count) => count + 1)}>
-          count is {count}
-        </button>
-        <p>
-          Edit <code>src/App.jsx</code> and save to test HMR
-        </p>
-      </div>
-      <p className="read-the-docs">
-        Click on the Vite and React logos to learn more
-      </p>
+      
+      <Button/>
+      <h1>AAAAAAAAA</h1>
     </>
-  )
+    
+  );
 }
 
-export default App
+export default App;
