d332db51945370e61f75c53a7971cf9a65525ce6
MarkovicMaks <markovic.maks@gmail.com>
2024-12-05 18:16:09 +0100

    Got the strap boot working

--- Student-recepie-app/src/Components/Navbar.jsx
+++ Student-recepie-app/src/Components/Navbar.jsx
@@ -3,7 +3,7 @@ import Nav from 'react-bootstrap/Nav';
 import Navbar from 'react-bootstrap/Navbar';
 import NavDropdown from 'react-bootstrap/NavDropdown';
 
-function NavbarRecepieApp() {
+function RecepieNavbar() {
   return (
     <Navbar expand="lg" className="bg-body-tertiary">
       <Container>
@@ -31,4 +31,4 @@ function NavbarRecepieApp() {
   );
 }
 
-export default NavbarRecepieApp;
+export default RecepieNavbar;
---
+++ Student-recepie-app/src/Pages/App.jsx
@@ -0,0 +1,15 @@
+import React from 'react';
+import RecepieNavbar from '../Components/Navbar';
+import RecepieCard from '../Components/RecepieCard';
+
+function App() {
+  return (
+    <>
+      <RecepieNavbar/>
+      <RecepieCard></RecepieCard>
+      <h1>BBB</h1>
+    </>
+  );
+}
+
+export default App;
