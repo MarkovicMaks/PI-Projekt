259769bdaabfec7026e9597755b5c43d9048e0c6
MarkovicMaks <markovic.maks@gmail.com>
2025-01-13 17:23:17 +0100

    re addin java spring cuz im dumb

---
+++ Backend/StudentRecepieApp/src/main/java/MaksMarkovic/Algebra/StudentRecepieApp/StudentRecepieAppApplication.java
@@ -0,0 +1,13 @@
+package MaksMarkovic.Algebra.StudentRecepieApp;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class StudentRecepieAppApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(StudentRecepieAppApplication.class, args);
+	}
+
+}
---
+++ Backend/StudentRecepieApp/src/test/java/MaksMarkovic/Algebra/StudentRecepieApp/StudentRecepieAppApplicationTests.java
@@ -0,0 +1,13 @@
+package MaksMarkovic.Algebra.StudentRecepieApp;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class StudentRecepieAppApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
