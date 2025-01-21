4a5f1b2509b0ebae12912b827b14c7b33a538578
MarkovicMaks <markovic.maks@gmail.com>
2025-01-21 13:13:45 +0100

    frontend

--- Student-recepie-app/vite.config.js
+++ Student-recepie-app/vite.config.js
@@ -1,6 +1,21 @@
-import { defineConfig } from 'vite'
-import react from '@vitejs/plugin-react'
+import { defineConfig } from 'vite';
+import react from '@vitejs/plugin-react';
 
 export default defineConfig({
   plugins: [react()],
-})
+  test: {
+    globals: true,
+    environment: 'jsdom', 
+    coverage: {
+      provider: 'c8', 
+      reportsDirectory: 'coverage', 
+      all: true, 
+      exclude: [
+        'node_modules/',
+        'src/**/*.test.{js,ts,jsx,tsx}',
+        'src/**/__tests__/**', 
+        'src/main.{js,ts,jsx,tsx}',
+      ],
+    },
+  },
+});
